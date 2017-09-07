1770ae13094cb77c08dd350819213bd490c0345d
Chris Morikawa <morikawac74@gmail.com>
2017-09-06 19:49:02 -0700

    Another One

---
+++ Roll A Ball Chris Morikawa/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+	//Control of the speed
+	public float speed;
+	//The Score of the Count
+	private int count;
+	//for the player settings to see
+	public Text countText;
+	//For the Win Text to appear;
+	public Text WinText;
+
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		WinText.text = "";
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Pick Up")) 
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 16)
+		{
+			WinText.text = "YOU WIN!";
+		}
+	}
+}
